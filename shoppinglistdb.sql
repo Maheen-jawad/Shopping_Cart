f9c989d14f7a78c7446c939932fbad2a91d5affd
Maheen-jawad <93900676+Maheen-jawad@users.noreply.github.com>
2021-11-21 14:19:21 +1100

    Add files via upload

--- shoppinglistdb.sql
+++ shoppinglistdb.sql
@@ -1,18 +1,31 @@
--- create and select the database
-DROP DATABASE IF EXISTS shoppinglistdb;
-CREATE DATABASE shoppinglistdb;
-USE shoppinglistdb;  
+-- Database: `shoppinglistdb`
 
--- Creating Table
-CREATE TABLE items (
-  itemNo       INT(11)        NOT NULL   AUTO_INCREMENT,
-  itemName     VARCHAR(255)   NOT NULL,
-  PRIMARY KEY (itemID)
-);
+-- Table structure for table `shopping_list`
+--
+
+CREATE TABLE `shopping_list` (
+  `item_no` int(11) NOT NULL,
+  `Item_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shopping_list`
+--
+
+INSERT INTO `shopping_list` (`item_no`, `Item_name`) VALUES
+(1, 'Butter');
+
+-- Indexes for table `shopping_list`
+--
+ALTER TABLE `shopping_list`
+  ADD PRIMARY KEY (`item_no`);
 
 
--- create the user and grant priveleges.
+ALTER TABLE `shopping_list`
+  MODIFY `item_no` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 GRANT SELECT, INSERT, DELETE, UPDATE
 ON shoppinglistdb.*
-TO usr@localhost
-IDENTIFIED BY 'pa55word'; 
+TO root@localhost
+IDENTIFIED BY '';
