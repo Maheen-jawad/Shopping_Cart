e177d5904e583b7eb559ad1edce28385cb240cb5
Maheen-jawad <93900676+Maheen-jawad@users.noreply.github.com>
2021-11-21 14:18:07 +1100

    Update item_list.php

--- item_list.php
+++ item_list.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
+         <!--Head Section-->
 <head>
-    <title>Shopping List Manager</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>SHOPPING LIST MANAGER</title>
+    <link rel="stylesheet" type="text/css" href="main2.css">
 </head>
+
+<!--Body Section-->
 <body>
     <header>
-        <h1>Shopping List Manager</h1>
+        <h1>SHOPPING LIST MANAGER</h1>
     </header>
+	
 	<main>
 	
-        <!--error message--!>
-	<?php if(count($errors) > 0):?>
-	<h2> errors: </h2>
-	<ul>
-	<?php foreach($errors as $error):?>
-	<li><?php echo $error;?></li>
-	<?php endforeach;?>
-	</ul>
-	<?php endif; ?>
+	<!--Error Message-->
+	<?php if (count($errors) > 0) : ?>
+        <h2>Errors:</h2>
+        <ul>
+            <?php foreach($errors as $error) : ?>
+                <li><?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
 	
-        <!-- Items list--!>
+	<!--Item Display List-->
 	<h2> Items: </h2>
-	<?php if($item_list == NULL):?>
+	<?php if(count($item_list) == 0):?>
 	<p>Shopping list is empty.</p>
 	<?php else:?>
 	<ul>
@@ -33,7 +37,7 @@
 	<?php endif;?>
 	<br>
 	
-        <!-- Adding items --!>
+	<!-- Add items form -->
 	<h2>Add Item:</h2>
 	<form action="." method="post">
 	<?php foreach ($item_list as $name):?>
@@ -46,7 +50,7 @@
 	</form>
 	<br>
 	
-         <!--Select Form--!>
+	<!-- Delete, Modify, Sort items form -->
 	 <?php if (count($item_list) > 0 && empty($item_to_modify)) : ?>
         <h2>Select Item:</h2>
         <form action="." method="post">
@@ -71,8 +75,6 @@
             <input type="submit" name="action" value="sort items">
         </form>
         <?php endif; ?>
-
-<!--Modify Form--!>
 <?php if (!empty($item_to_modify)) : ?>
         <h2>Item to Modify:</h2>
         <form action="." method="post">
@@ -81,16 +83,22 @@
             <?php endforeach; ?>
             <label>Item:</label>
             <input type="hidden" name="item_to_modify" value="<?php echo $item_to_modify; ?>">
-            <input type="text" name="itemid" value="<?php echo $modify_item; ?>">
-            <label>&nbsp;</label>
-            <input type="submit" name="action" value="Save Changes">
+            <input type="text" name="modifieditem" value="<?php echo $item_to_modify; ?>">
+			
+			<!-- Modified items Save/cancel button-->
+           <label>&nbsp;</label>
+            <input type="submit" name="action"  value="Save Changes">
             <input type="submit" name="action" value="Cancel Changes">
+			
         </form>
         <?php endif; ?>
-		
 		    </main>
+			
+			<!--Footer Section-->
+			<footer>
+    <p>&copy; <?php echo date("Y"); ?> Shopping List Manager</p>
+</footer>
+
 </body>
 </html>
-	</main>
-	</body>
-	</html>
+	
